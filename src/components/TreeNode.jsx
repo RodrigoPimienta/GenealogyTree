5c9a035f10b640bec1f8fc18427ac8516f9cbb06
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-20 20:44:56 -0600

    fix language

--- src/components/TreeNode.jsx
+++ src/components/TreeNode.jsx
@@ -1,24 +1,23 @@
-
-export const TreeNode = ({node, toggleExpand}) => {
-    const statusColor = node.data.status === 'Active' ? 'green' : 'red';
+export const TreeNode = ({ node, toggleExpand }) => {
+  const statusColor = node.data.status === 'Active' ? 'green' : 'red';
   return (
-        <div className="node-container">
-          <div className="node-header" style={{ color: statusColor }}>
-            {node.label}
-          </div>
-          <div className="node-content">
-            <div><strong>{node.data.name}</strong></div>
-            {node.expanded && (
-              <>
-                <div><strong>Producto:</strong> {node.data.producto}</div>
-                <div><strong>Categoría:</strong> {node.data.categoria}</div>
-                <div><strong>Estado:</strong> <span style={{ color: statusColor }}>{node.data.status}</span></div>
-              </>
-            )}
-            <button onClick={() => toggleExpand(node)} style={{ marginTop: '10px' }}>
-              {node.expanded ? 'Ocultar detalles' : 'Ver detalles'}
-            </button>
-          </div>
-        </div>
+    <div className="node-container">
+      <div className="node-header" style={{ color: statusColor }}>
+        {node.label}
+      </div>
+      <div className="node-content">
+        <div><strong>{node.data.name}</strong></div>
+        {node.expanded && (
+          <>
+            <div><strong>Product:</strong> {node.data.producto}</div>
+            <div><strong>Category:</strong> {node.data.categoria}</div>
+            <div><strong>Status:</strong> <span style={{ color: statusColor }}>{node.data.status}</span></div>
+          </>
+        )}
+        <button onClick={() => toggleExpand(node)} style={{ marginTop: '10px' }}>
+          {node.expanded ? 'Hide details' : 'Show details'}
+        </button>
+      </div>
+    </div>
   )
 }
